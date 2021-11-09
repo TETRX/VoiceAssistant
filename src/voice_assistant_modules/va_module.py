ddaeda9359e14dbf8fa0960ecfc6988ff807d5dd
Wojciech <w.wegrzynek@gmail.com>
2021-11-09 20:52:02 +0100

    Logging

--- src/voice_assistant_modules/va_module.py
+++ src/voice_assistant_modules/va_module.py
@@ -5,16 +5,21 @@ from abc import ABC
 import paho.mqtt.client as mqtt
 
 from src.config.network_config import QUERY_CHANNEL, ANSWER_CHANNEL, DEFAULT_BROKER
+from src.voice_assistant_modules.exchange import Exchange
+from src.voice_assistant_modules.module_observer import ModuleObserver
+from src.voice_assistant_modules.timer import Timer
 
 
 class VAModule(ABC):
     def __init__(self, broadcast_channel=ANSWER_CHANNEL, listen_channel=QUERY_CHANNEL, broker=DEFAULT_BROKER,
-                 client=mqtt.Client()):
+                 client=mqtt.Client(), timer = Timer()):
         self.broadcast_channel = broadcast_channel
         self.listen_channel = listen_channel
         self.broker = broker
         self.client = client
         self.init_client()
+        self.observers = []
+        self.timer = timer
 
     def init_client(self):
         self.client.on_message = self.on_message
@@ -29,16 +34,27 @@ class VAModule(ABC):
         self.client.loop_start()
 
     def on_message(self, client, userdata, message):
+        time_received = self.timer.get_curr_time()
         query = str(message.payload.decode("utf-8"))
-        print("Received:", query)
         answer = self.process_query(query)
-        print("Answering", query, "with:", answer)
+        time_answered = self.timer.get_curr_time()
+
+        exchange = Exchange(query, answer, time_received=time_received, time_answered=time_answered)
+        for observer in self.observers:
+            observer.notify(exchange)
+
         if answer is not None:
             self.client.publish(self.broadcast_channel, answer)
+
+    def get_name(self):
+        return type(self).__name__
 
     @abc.abstractmethod
     def process_query(self, query: string) -> string:
         pass
+
+    def observe(self, observer : ModuleObserver):
+        self.observers.append(observer)
 
     @classmethod
     def main(cls):
@@ -47,5 +63,10 @@ class VAModule(ABC):
         if len(sys.argv) >= 2:
             broker = sys.argv[1]
         module = cls(broker=broker)
+
+        from ..logging.module_logger import ModuleLogger
+        module_logger = ModuleLogger(module)
+        module.observe(module_logger)
+
         import time
         time.sleep(10000)  # TODO: better solution
