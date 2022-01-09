82c01f3610d1c8795a9262549394e24f40330d5b
Wojciech <w.wegrzynek@gmail.com>
2022-01-09 15:13:09 +0100

    Updated EchoModule

--- src/voice_assistant_modules/echo_module/echo_module.py
+++ src/voice_assistant_modules/echo_module/echo_module.py
@@ -2,6 +2,10 @@ from src.voice_assistant_modules.va_module import VAModule
 
 
 class EchoModule(VAModule):
+    @classmethod
+    def get_id(cls):
+        return "echo"
+
     def process_query(self, query: str) -> str:
         return query
 
